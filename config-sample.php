2cf7b5f346576304e6b6987ffca76d8803864fb6
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 20:36:30 +0300

    Updated

---
+++ config-sample.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Copy as config.php and change settings */
+
+define('SQL_HOSTNAME', 'localhost');
+define('SQL_USERNAME', 'infodesk');
+define('SQL_PASSWORD', '12345678');
+define('SQL_DATABASE', 'infodesk');
+define('SQL_PREFIX', 'infodesk_');
+
+return;
+?>
