a12bc9a9263e775878b10e88b32942f03d59ad1e
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 17:36:27 +0300

    Updated

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+/* Main code */
+
+/* RequireJS configurations */
+requirejs.config({
+	shim: {
+		'prototype':{
+			deps: [],
+			exports: 'Ajax'
+		},
+		'bootstrap':{
+			deps: ['jquery']
+		}
+	}
+});
+
+/* Post message to server */
+function post_message(args) {
+	var message = args.message || '';
+	require(["prototype"], function(Ajax) {
+		new Ajax.Request('postmsg.php', {
+		    method:'post',
+		    onSuccess: function(transport){
+				var response = transport.responseText || "no response text";
+				alert("Success! \n\n" + response);
+		    },
+		    onFailure: function(){ alert('Something went wrong...') }
+		  });
+	});
+}
+
+/* Post message to server */
+function post_message_form(button) {
+	var form = button.form;
+	post_message({'message': form.message.value});
+}
+
+/* Init everything at onLoad event */
+window.onload = function(){
+	
+	// Load Bootstrap
+	require(["bootstrap"], function() {
+		
+	});
+	
+	// TODO: Setup simple clock on control form
+	
+	// TODO: Setup previous event history
+	
+	// TODO: Start fetching new events
+}
+
+/* EOF */
