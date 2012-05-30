6cc5a007136ed179583021b27a66d2c6540996a7
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 21:04:56 +0300

    Updated

--- backend.php
+++ backend.php
@@ -7,7 +7,8 @@ try {
 	}
 	set_error_handler("myErrorHandler");
 	function my_exception_handler($e) {
-		header("Status: 400 Bad Request\nContent-Type:text/plain");
+		header("Content-Type: text/plain");
+		header("Status: 400 Bad Request");
 		echo 'Exception: ' . $e->getMessage() . "\n"
 		   . '@code: ' . $e->getCode() . "\n"
 		   . '@file: ' . $e->getFile() . ':' . $e->getLine() . "\n"
