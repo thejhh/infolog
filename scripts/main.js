99b2b4519583e286f53ba3c4b56717f91c4d8835
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-31 01:54:20 +0300

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -123,7 +123,7 @@ function update_events() {
 				msg = jquery('<div/>').text(event.msg).html();
 				msg = msg.replace(/#([a-zA-Z0-9]+)/, function($0, $1) {
 					var hashtag = ''+$1;
-					return jquery('<div><a class="label label-info">#' + $1 + '</a></div>').click(function() {
+					return jquery('<a class="label label-info">#' + $1 + '</a>').click(function() {
 						change_to_hashtag(hashtag);
 					}).html();
 				});
