fe2a251a88380a5f4e61ff1dcc84ecea205cc849
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 18:08:26 +0300

    Updated

--- postmsg.php
+++ postmsg.php
@@ -5,6 +5,7 @@ try {
 
 	throw new Exception('Unknown request');
 } catch(Exception $e) {
+	header('Content-Type: text/plain');
 	echo 'FAIL: ' . $e->getMessage() . "\n"
 	   . ' at file: ' . $e->getFile() . ':' . $e->getLine() . "\n"
 	   . ' with stack:'."\n"
