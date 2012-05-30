f84344ddc7aca9135b057d7c5d5465f841c7ae13
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 18:08:05 +0300

    Updated

--- postmsg.php
+++ postmsg.php
@@ -5,7 +5,10 @@ try {
 
 	throw new Exception('Unknown request');
 } catch(Exception $e) {
-	echo 'FAIL: ' . $e;
+	echo 'FAIL: ' . $e->getMessage() . "\n"
+	   . ' at file: ' . $e->getFile() . ':' . $e->getLine() . "\n"
+	   . ' with stack:'."\n"
+	   . $e->getTrace();
 }
 return;
 ?>
