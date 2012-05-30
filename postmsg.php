148b5ea25d1b2e730ddd66c38310427b3906a0e1
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 18:09:27 +0300

    Updated

--- postmsg.php
+++ postmsg.php
@@ -7,8 +7,8 @@ try {
 } catch(Exception $e) {
 	header('Content-Type: text/plain');
 	echo 'FAIL: ' . $e->getMessage() . "\n"
-	   . ' at file: ' . $e->getFile() . ':' . $e->getLine() . "\n"
-	   . ' with stack:'."\n"
+	   . '@file: ' . $e->getFile() . ':' . $e->getLine() . "\n"
+	   . '@stack:'."\n"
 	   . $e->getTrace();
 }
 return;
