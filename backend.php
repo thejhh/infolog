1aec1747c9afb7b807d8c97fcf95b52a01ffca88
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-31 00:08:48 +0300

    Updated

--- backend.php
+++ backend.php
@@ -42,13 +42,13 @@ try {
 	class SQL {
 		static private $mysqli = null;
 		static public function init() {
-			if(is_null($this->mysqli)) {
-				$this->mysqli = mysqli_connect(SQL_HOSTNAME, SQL_USERNAME, SQL_PASSWORD, SQL_DATABASE);
-				if($this->mysqli->connect_error) {
+			if(is_null(self::$mysqli)) {
+				self::$mysqli = mysqli_connect(SQL_HOSTNAME, SQL_USERNAME, SQL_PASSWORD, SQL_DATABASE);
+				if(self::$mysqli->connect_error) {
 					throw new Exception('SQL connection failed: ' . $this->mysqli->connect_error);
 				}
 			}
-			return $this->mysqli;
+			return self::$mysqli;
 		}
 	}
 
