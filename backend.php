66596e780ce0239515b0137f24a36e2d134ecde5
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-30 19:29:19 +0300

    Updated

--- backend.php
+++ backend.php
@@ -5,7 +5,7 @@ try {
 
 	throw new Exception('Unknown request');
 } catch(Exception $e) {
-	header('Content-Type: text/plain');
+	header("Status: 500 Internal Server Error\nContent-Type: text/plain");
 	echo 'Exception: ' . $e->getMessage() . "\n"
 	   . '@code: ' . $e->getCode() . "\n"
 	   . '@file: ' . $e->getFile() . ':' . $e->getLine() . "\n"
