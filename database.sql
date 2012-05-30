48a9607de887dbd6bb71a85cd25aa2b86ef4897e
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-31 02:32:05 +0300

    Updated

--- database.sql
+++ database.sql
@@ -2,6 +2,6 @@ CREATE TABLE `infodesk_log` (
     log_id           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     updated          TIMESTAMP NOT NULL DEFAULT 0,
     created          TIMESTAMP NOT NULL DEFAULT 0,
-    msg             VARCHAR(255) NOT NULL DEFAULT '',
+    msg              TEXT NOT NULL DEFAULT '',
     PRIMARY KEY(log_id)) CHARACTER SET utf8 ENGINE=InnoDB;
 
