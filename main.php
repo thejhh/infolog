92390570127b450fa4a2769bff05a7fa6fdd7a47
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-06-01 12:39:45 +0300

    Updated

--- main.php
+++ main.php
@@ -1,17 +1,49 @@
 <?php
+	// Set PHP settings
 	error_reporting(E_ALL|E_STRICT);
 
-	function myErrorHandler($errno, $errstr, $errfile, $errline) {
+	/* */
+	class ErrorIdentifier {
+		static private $id = 0;
+		static public function create($data) {
+			$key = '';
+			if(isset($data['error'])) {
+				$key .= $data['error'];
+				if(isset($data['file'])) $key .= '|' . $data['file'];
+				if(isset($data['line'])) $key .= '|' . $data['line'];
+			} else {
+				$key = json_encode($data);
+			}
+			self::$id++;
+			return sha1($key) + '-' + self::$id;
+		}
+	}
+
+	/* Write to stderr */
+	function writeln_stderr($msg) {
+		file_put_contents("php://stderr", $msg . "\n");
+	}
+
+	/* Write to stderr */
+	function output_error_object(array $data) {
+		$json = json_encode($data);
+		writeln_stderr($json);
 		header("Content-Type: application/javascript");
 		header("Status: 400 Bad Request");
-		echo json_encode(array("message" => "$errno $errstr at $errfile:$errline"));
+		echo json_encode(array('error' => 'Internal error with identifier ' . ErrorIdentifier::get($data))) . "\n";
 		exit;
 	}
-	set_error_handler("myErrorHandler");
+
+	/* PHP error handler */
+	function my_error_handler($errno, $errstr, $errfile, $errline) {
+		output_error_object(array("error" => "$errno $errstr at $errfile:$errline"));
+	}
+
+	/* Exception handler */
 	function my_exception_handler($e) {
 		try {
 			$data = array(
-				'message' => ''.$e->getMessage(),
+				'error' => ''.$e->getMessage(),
 				'code' => $e->getCode(),
 				'file' => ''.$e->getFile(),
 				'line' => $e->getLine()
@@ -23,21 +55,22 @@
 				foreach($stack as $tmp) { $data['trace'][] = $tmp; }
 			}
 
-			header("Content-Type: application/javascript");
-			header("Status: 400 Bad Request");
-			echo json_encode($data) . "\n";
+			output_error_object($data);
 		} catch(Exception $e2) {
-			header("Status: 400 Bad Request");
-			echo '{message="Got another exception `' . $e2 . '` while handling exception `' . $e . '`"}';
+			output_error_object(array('error'=>"Got another exception '" . $e2 . "' while handling exception '" . $e . "'"));
 		}
 		exit;
 	}
+
+	// Set handlers
+	set_error_handler('my_error_handler');
 	set_exception_handler('my_exception_handler');
 
+	// Read config
 	if(!file_exists(dirname(__FILE__) . '/config.php')) { throw new Exception('No configuration!'); }
 	require_once(dirname(__FILE__) . '/config.php');
 
-	/* Setup MySQL */
+	/* Setup JIT MySQL */
 	class SQL {
 		static private $mysqli = null;
 		static public function init() {
