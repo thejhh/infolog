3596101b12582fa5f41c6de8f8c55d3dc0e62ad3
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-06-01 20:01:40 +0300

    Updated

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 	function __autoload($class_name) {
-		require_once( dirname(__FILE__) . '/' . $class_name . '.php');
+		require_once( dirname(__FILE__) . '/classes/' . $class_name . '.php');
 	}
 
 	// Set PHP settings
