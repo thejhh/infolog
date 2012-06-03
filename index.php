21e16cba353e4a2a801193f359066436cbf0338a
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-06-03 19:57:51 +0300

    Updated

--- index.php
+++ index.php
@@ -189,7 +189,7 @@
 		</div>
 
 		<div id="site-generator">
-			Proudly powered by <a href="https://github.com/jheusala/infolog#infolog" title="Infolog.in" rel="generator">Infolog.in</a>.
+			Proudly powered by <a href="https://github.com/jheusala/infolog#infolog" title="Infolog.in" rel="generator">Infolog.in</a>
 		</div>
 
 		<a target="_blank" href="https://github.com/jheusala/infolog"><img style="position: absolute; top: 60px; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
